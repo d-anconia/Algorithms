9c47c624af8c759c34a06bceb5400d7b5572b8b2
Danil Gorchakov <Gorchakov@phystech.edu>
2016-09-15 17:22:41 +0300

    Rejected commit: Default

--- src/com/danil/gorchakov/Insertion.java
+++ src/com/danil/gorchakov/Insertion.java
@@ -7,11 +7,10 @@ public class Insertion {
     public static void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 1; i < N; i++) {
-            for (int j = i; j > 0 && less(a[j], a[j - 1); j--)
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--)
                 exch(a, j, j - 1);
         }
     }
-
 
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
